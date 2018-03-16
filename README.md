fe727d8eda23bb279ca1122a5f34a59bd988817c
Mata Salinas <c_matasalinas@tss-hr420-dm21.fc.ca>
2018-03-16 11:51:20 -0400

    class finished still working

---
+++ README.md
@@ -0,0 +1,2 @@
+# Vue Video
+In the following folder you can find the first class of vue
--- js/main.js
+++ js/main.js
@@ -1,26 +1,31 @@
 const vm = new Vue({
   el : "#app", // el = element. specify which section should it affect, this case it affects the "app" id secyion
   data: {
-    welcomeMessage : "Welcome to the first vue",
+    mainmessage : "what up",
 
-    deliciousFruit : [
-      {name : "apple", flavour : "tasty"},
-      {name : "pen", flavour : "sweet"},
-      {name : "pineapple", flavour : "tangy"}
+    videodata : [
+      {name: "Star Wars the Force Awakens", thumb: "forceawakens.jpg", vidsource: "forceawakens.mp4", description: "another SW movie"},
+      {name: "Stranger Things", thumb: "strangerthings.jpg", vidsource: "strangerthings.mp4", description: "ze upside downnnn"},
+      {name: "Marvel's Avengers", thumb: "avengers.jpg", vidsource: "avengers.mp4", description: "avengers UNIDOS JAMAS SERAN VENCIDOS"}
+
     ],
-    result : "penpineappleapplepen",
 
-    hasVue : false,
-    vuemessage: "hey, you can haz vue",
-
-    seen : true
+    videotitle : "title goes here",
+    videodescription : "description here, yay",
+    videosource : ""
   },
+
   methods : {
-    logClicked(e) {
-      console.log(e.currentTarget, this);
-    },
-    reversemess : function() {
-      this.result = this.result.split('').reverse().join('')
+    loadMovie(e) {
+      e.preventDefault();
+
+      dataKey = e.currentTarget.href.substring(e.currentTarget.href.lastIndexOf('/') +1 );
+
+      currentData = this.videodata.filter((video) => video.vidsource === dataKey);
+
+      this.videotitle = currentData[0].name;
+      this.videodescription = currentData[0].description;
+      this.videosource = dataKey;
     }
   }
 });
